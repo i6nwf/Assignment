2033b35db2f23c8dde41c0c80cc6f54c0c352268
i6nwf <151959117+i6nwf@users.noreply.github.com>
2023-12-09 16:16:47 +0300

    Delete Eighth.java

--- Eighth.java
+++
@@ -1,9 +0,0 @@
-public class Eighth {
-//    2.12Draw a class inheritance diagram for the following set of classes:
-//    • Class Goat extends Object and adds an instance variable tail and methods milk( ) and jump( ).
-//    • Class Pig extends Object and adds an instance variable nose and methods eat(food) and wallow( ).
-//    • Class Horse extends Object and adds instance variables height and color, and methods run( ) and jump( ).
-//    • Class Racer extends Horse and adds a method race( ).
-//    • Class Equestrian extends Horse and adds instance variable weight and isTrained, and methods trot( ) and isTrained( ).
-//    Word Doc.
-}
