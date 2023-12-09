aeeede109fee5d31d07ee06c0e4863201d01f451
i6nwf <151959117+i6nwf@users.noreply.github.com>
2023-12-09 16:19:35 +0300

    Delete forth.java

--- forth.java
+++
@@ -1,6 +0,0 @@
-public class forth {
-//    2.8 Can two interfaces mutually extend each other? Why or why not?
-//    Two interfaces cannot mutually extend each other.
-//    it causes Cyclic inheritance
-
-}
